340b3dfd3c830df53308eb48bf41241f8ca21755
Matthieu <115145423+matthieu-geley@users.noreply.github.com>
2023-11-10 10:34:03 +0100

    Update bonusSecond.py
    Added short description of each methods

--- BonusJobs/bonusSecond.py
+++ BonusJobs/bonusSecond.py
@@ -2,6 +2,12 @@ input_list = [64, 34, 12, 25, 22, 11, 90]
 
 
 def bubbleSort(list):
+    """
+    Le tri à bulles est un algorithme de tri qui parcourt répétitivement une liste qui doit être triée,
+    qui compare chaque paire d'éléments adjacents et les échange si ils sont dans le mauvais ordre.
+    Ce passage à travers la liste est répété jusqu'à ce qu'il n'y ait plus d'échange.
+    Ce tri est nommé suite à la façon dont les nombres se déplacent à travers la liste qui ressemble à des bulles qui remontent à la surface de l'eau.
+    """
     for i in list:
         for j in range(len(list)-1):
             if list[j] > list[j+1]:
@@ -10,6 +16,10 @@ def bubbleSort(list):
             return list
 
 def insertionSort(list):
+    """
+    Le tri par insertion est un algorithme de tri qui place chaque élément de la liste à sa place appropriée dans la liste triée.
+    C'est le tri le plus utilisé dans la vie réelle, car il est très efficace pour les petites tailles (exemple : tri des cartes à jouer)
+    """
     for i in range(len(list)-1):
         x = list[i]
         j = i
@@ -21,6 +31,10 @@ def insertionSort(list):
         return list
 
 def fusionSort(list):
+    """
+    Le tri fusion est un algorithme de tri récursif qui divise chaque liste en deux moitiés (jusqu'à n'obtenir que des éléments solitaire),
+    trie chacun des éléments , puis fusionne les deux moitiés triées.
+    """
     if len(list) > 1:
         mid = len(list)//2
         left = list[:mid]
@@ -47,7 +61,17 @@ def fusionSort(list):
         # print(list)
         return list
 
+
+
+
 def partList(list, low, high, pivot):
+    """
+    low = valeur basse
+    high = valeur haute
+    pivot = valeur de référence
+
+    Fractionne la liste en deux parties, une partie avec des valeurs plus petites que le pivot et l'autre avec des valeurs plus grandes que le pivot
+    """
     i = low - 1
     for j in range(low, high):
         if list[j] <= pivot:
@@ -57,6 +81,10 @@ def partList(list, low, high, pivot):
     return i+1
 
 def quickSort(list, first, last):
+    """
+    first = première valeur
+    last = dernière valeur
+    """
     if first < last:
         pivot = list[last]
         part = partList(list, first, last, pivot)
